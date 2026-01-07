fa3a8472aed88a719b49de6122c61f64bea2faf3
Hubert <hubert2recovery1@outlook.com>
2026-01-07 13:59:24 +0000

    Fix bug involving missing brackets

--- Tame Impala song guessing/THE GAME.py
+++ Tame Impala song guessing/THE GAME.py
@@ -4,4 +4,4 @@ root=tk.Tk()
 root.geometry('300x500')
 b1=tk.Button(root,
              text='Click me',
-             command=lambda: print('Button clicked!')
+             command=lambda: print('Button clicked!'))
