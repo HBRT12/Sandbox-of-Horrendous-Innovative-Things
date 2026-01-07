15f67a8a3d0b2e34f8ef5b3710ec9c5244406f0a
Hubert <hubert2recovery1@outlook.com>
2026-01-07 11:20:23 +0000

    Add comments and final input

--- circle.py
+++ circle.py
@@ -1,11 +1,13 @@
 import turtle
 
-t=turtle.Turtle()
+t=turtle.Turtle()  # Setting up objects for turtle
 s=turtle.Screen()
 
-s.bgcolor("white")
+s.bgcolor("white")  # Setting colours for bg and line
 t.color("black")
 
 for i in range(360):
     t.forward(1)
     t.left(1)
+
+input("Press enter to exit")  # Awaits user input before quitting
